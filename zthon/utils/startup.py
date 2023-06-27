782598eaf17d3807d38abc00fd7dbcb4f4720963
biilal1 <135733490+biilal1@users.noreply.github.com>
2023-06-27 16:08:28 +0300

    Update startup.py

--- zthon/utils/startup.py
+++ zthon/utils/startup.py
@@ -294,7 +294,7 @@ async def mybot():
 
             await asyncio.sleep(1)
 
-            await bot.send_file("@BotFather", "zthon/BILAL/logozed.jpg")
+            await bot.send_file("@BotFather", "zthon/zilzal/logozed.jpg")
 
             await asyncio.sleep(3)
 
